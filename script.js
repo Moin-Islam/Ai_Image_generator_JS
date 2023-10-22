e147a29237f6c1ceab4db2abdd5b46f78d7efdb9
Moin-Islam <pkarnival@gmail.com>
2023-10-22 18:56:28 +0600

    2nd commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const generateForm = document.querySelector(".generate-form");
+const imageGallery = document.querySelector(".image-gallery");
+
+const OPENAI_API_KEY = "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";
+
+
+const updateImageCard = (imgDataArray) =>{
+    imgDataArray.foreach((imgObject, index)=> {
+        const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+        const imgElement = imgCard.querySelector("img");
+
+        const aiGeneratedImg = `data:image/jpeg;base64,${imgObject.b64_json}`;
+        imgElement.src = aiGeneratedImg;
+
+        imgElement.onload = () => {
+            imgCard.classList.remove("loading");
+        }
+    });
+}
+
+
+const generateAiImages = async (userPrompt,userImgQuantity) => {
+    try{
+        const response = await fetch("https://api.openai.com/v1/images/generations", {
+            method : "POST",
+            headers : {
+                'Content-type' : "application/json",
+                'Authorization' : `Bearer ${OPENAI_API_KEY}`
+            },
+            body : JSON.stringify({
+                prompt : userPrompt,
+                n: parseInt(imageGallery),
+                size : "512x512",
+                response_format: "b64_json"
+            })
+        });
+
+        if(!response.ok) throw new Error("Failed to generate images! Please try again");
+
+        const {data} = await response.json();
+        console.log(data);
+        updateImageCard([...data]);
+
+    }catch (error){
+        alert(error.message);
+    }
+}
+
+const handleFormSubmission = (e) => {
+  e.preventDefault();
+
+  const userPrompt = e.srcElement[0].value;
+  const userImgQuantity = e.srcElement[1].value;
+
+  const imgCardMarkup = Array.from(
+    { length: userImgQuantity },
+    () =>
+      `<div class="img-card loading">
+        <img src="images/loader.svg" alt="image">
+        <a href="#" class="download-btn">
+            <img src="images/download.svg" alt="download icon">
+        </a>
+      </div>`
+  ).join("")
+
+  imageGallery.innerHTML = imgCardMarkup;
+  generateAiImages(userPrompt, userImgQuantity);
+};
+generateForm.addEventListener("submit", handleFormSubmission);
